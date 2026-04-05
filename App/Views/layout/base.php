4d82a130a4ffb106bf93bd406b322b2cfa24aef1
BarclaisC <barclaisconsulting@gmail.com>
2026-04-05 13:49:47 +0200

    Correction chemin CSS + mise à jour base.php

--- App/Views/layout/base.php
+++ App/Views/layout/base.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 
     <!-- Ton CSS (chemin corrigé) -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
